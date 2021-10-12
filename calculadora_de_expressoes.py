fd1d530c7408f28c2b12c6dea4e7b5fcfc33c965
Isaac Martins <91567120+IsaacMartins12@users.noreply.github.com>
2021-10-12 19:37:25 -0400

    Update calculadora_de_expressoes.py

--- calculadora_de_expressoes.py
+++ calculadora_de_expressoes.py
@@ -1,150 +1,175 @@
-import random   # importa a biblioteca que sorteia números
+def deletar (aux,pos,exp3,indice,valor) :
 
-def usou() :   # função que checa as letras que ja foram usadas
- print ("LETRAS QUE JÁ USOU : " ,end=" ")
- for i in range(0,len(showletter),1) :
-   print(showletter[i] , end=" ")
+     del(aux[pos-1:pos+2])
+     del(exp3[pos-1+indice:indice+pos+2])
+     aux.insert(pos-1, valor)
+     exp3.insert(pos-1+indice, valor)
+     for i in range (0,len(exp3)) :
+       print(exp3[i] , end = ' ') 
+     print('\n')
 
-def linhas() :     #função que mostras as linhas , preenchidas ou nao
-  for i in range(0,word_size,1) :
-   print(compare_word[i] , end=" ")
-  print("\n")
+def somar (exp3,indice,aux,op) :  
+          if op ==1 :
+            pos = aux.index('+')
+            valor = round((float(aux[pos-1]) + float(aux[pos+1])),3)
+            deletar(aux,pos,exp3,indice,valor)
+            return valor
+          else :
+             pos = exp3.index('+')
+             valor = round((float(exp3[pos-1]) + float(exp3[pos+1])),3)
+             del(exp3[pos-1:pos+2])
+             exp3.insert(pos-1, valor)
+             for i in range (0,len(exp3)) :
+                print(exp3[i] , end = ' ')
+             print('\n')
+             return valor
+def subtrair (exp3,indice,aux,op) :
+          if op == 1 :
+            pos = aux.index('-')
+            valor = round((float(aux[pos-1]) - float(aux[pos+1])),3)
+            deletar(aux,pos,exp3,indice,valor) 
+            return valor
+          else : 
+             pos = exp3.index('-')
+             valor = round((float(exp3[pos-1]) - float(exp3[pos+1])),3)
+             del(exp3[pos-1:pos+2])
+             exp3.insert(pos-1, valor)
+             for i in range (0,len(exp3)) :
+                print(exp3[i] , end = ' ')
+             print('\n')
+             return valor
 
-def mostrafase1() :
- print ("--"*5)
- print ("|"+" "*8 + "|")
- print ("|" + " "*8 + "O")
- print ("|\n"*4)
- usou()
- print ("\n\n")
- print (" "*3 )
- linhas()
- read1 = str(input('\n Informe sua letra :')).strip()
- print("\n" *15 )
- return(read1)
+def multiplicar (exp3,indice,aux,op) :
 
-def mostrafase2():
- print ("--"*5)
- print ("|"+" "*8 + "|")
- print ("|" + " "*8 + "O")
- print ("|" + " "*8 + "|")
- print ("|\n"*3)
- usou()
- print ("\n\n")
- print (" "*3 )
- linhas()
- read2 = str(input('\n Informe sua letra :')).strip()
- print("\n" *15 )
- return(read2)
+          if op == 1 :
+            pos = aux.index('*')
+            valor = round((float(aux[pos-1]) * float(aux[pos+1])),3)
+            deletar(aux,pos,exp3,indice,valor) 
+            return valor
+          else :
+             pos = exp3.index('*')
+             valor = round((float(exp3[pos-1]) * float(exp3[pos+1])),3)
+             del(exp3[pos-1:pos+2])
+             exp3.insert(pos-1, valor)
+             for i in range (0,len(exp3)) :
+                print(exp3[i] , end = ' ')
+             print('\n')
 
-def mostrafase3():
- print ("--"*5)
- print ("|" + " "*8 + "|")
- print ("|" + " "*8 + "O")
- print ("|" + " "*8 + "|")
- print ("|" + " "*7 + "/")
- print ("|\n"*3)
- usou()
- print ("\n\n")
- print (" "*3 )
- linhas()
- read3 = str(input('\n Informe sua letra :')).strip()
- print("\n" *15 )
- return(read3)
+def dividir (exp3,indice,aux,op) :
 
-def mostrafase4():
-    print ("--"*5)
-    print ("|" + " "*8 + "|")
-    print ("|" + " "*8 + "O")
-    print ("|" + " "*8 + "|")
-    print ("|" + " "*7 + "/ " + "\\")
-    print ("|" + " "*8 )
-    print ("|\n"*3)
-    usou()
-    print ("\n\n")
-    print (" "*3 )
-    linhas()
-    read4 = str(input('\n Informe sua letra :')).strip()
-    print("\n" *15 )
-    return(read4)
+          if op == 1 :
+            pos = aux.index('/')
+            valor = round((float(aux[pos-1]) / float(aux[pos+1])),3)
+            deletar(aux,pos,exp3,indice,valor) 
+            return valor
+          else : 
+             pos = exp3.index('/')
+             valor = round((float(exp3[pos-1]) / float(exp3[pos+1])),3)
+             del(exp3[pos-1:pos+2])
+             exp3.insert(pos-1, valor)
+             for i in range (0,len(exp3)) :
+                print(exp3[i] , end = ' ')
+             print('\n')
+             return valor
 
-def mostrafase5():
-    print ("--"*5)
-    print ("|" + " "*8 + "|")
-    print ("|" + " "*8 + "O")
-    print ("|" + " "*7 + "/" + "|")
-    print ("|" + " "*7 + "/ " + "\\")
-    print ("|" + " "*8 )
-    print ("|\n"*3)
-    print ("\n\n")
-    usou()
-    print ("\n\n")
-    print (" "*3 )
-    linhas()
-    read5 = str(input('\n Informe sua letra :')).strip()
-    print("\n" *15 )
-    return(read5)
+def potencia (exp3,indice,aux,op) :
 
-def mostrafase6():
-    print ("--"*5)
-    print ("|" + " "*8 + "|")
-    print ("|" + " "*8 + "O")
-    print ("|" + " "*7 + "/" + "|" + "\\")
-    print ("|" + " "*7 + "/ " + "\\")
-    print ("|" + " "*8 )
-    print ("|\n"*3)
-    usou()
-    print ("\n\n")
-    print (" "*3 )
-    print("---> ",end="")
-    linhas()
-    print("\n")
+          if op == 1 :
+            pos = aux.index('^')
+            valor = round((float(aux[pos-1]) ** float(aux[pos+1])),3)
+            deletar(aux,pos,exp3,indice,valor) 
+            return valor
+          else : 
+             pos = exp3.index('^')
+             valor = round((float(exp3[pos-1]) ** float(exp3[pos+1])),3)
+             del(exp3[pos-1:pos+2])
+             exp3.insert(pos-1, valor)
+             for i in range (0,len(exp3)) :
+                print(exp3[i] , end = ' ')
+             print('\n')
+             return valor
 
-def mostrafase0():
- print ("--"*5 )
- print ("|"+" "*8 + "|")
- print ("|\n"*5)
- usou()
- print ("\n\n" + " "*3)
- linhas()
- read = str(input('\n Informe sua letra :')).strip()
- print("\n" *15 )
- return(read)
-
-showletter=[]
-word_choice=[]
-compare_word = []
-
-gameover=acertou=0
-print('[1] ANIMAIS \n[2] PAÍSES  ')
-op = int(input('ESCOLHA A CATEGORIA : '))
-if (op==1) :
- words = ['macaco','arara','urso','peixe','cachorro','gato','pelicano','tubarao','avestruz','tartaruga','jabuti', 'orangotango','camarao','cavalo','tigre','onça','foca','baleia','rinoceronte','gaivota','hamster']
-if (op==2) :
-   words = ['alemanha','portugal','brasil','argetina','chile','uruguai','peru','marrocos','turquia','arzebaijao','mexico', 'espanha','italia','finlandia','franca','holanda','nigeria','cuba','india','china','japao']
-if (op==1 or op==2) :
- word_index = random.randint(0,21)   #sorteia um indice da lista
- word_size = len(words[word_index])  #pega o tamanho da palavra escohida
- word_choice = words[word_index]    #pega a palavra escolhida
- for i in range (0,word_size,1) :
-  compare_word += "_"
+def conta_especial(exp1,indice) :
+ op = 1
+ if '(' in exp1 and ')' in exp1 : aux = exp1[exp1.index('(')+1:exp1.index(')')]
+ elif '[' in exp1 and ']' in exp1 : aux = exp1[exp1.index('[')+1:exp1.index(']')]
+ elif '{' in exp1 and '}' in exp1 : aux = exp1[exp1.index('{')+1:exp1.index('}')]
+ while(True) :
+   if '*' in aux and '/' in aux :
+    if aux.index("*") < aux.index("/") :
+        if '^' in aux : valor = potencia(exp1,indice,aux,op) 
+        if '*' in aux : valor = multiplicar(exp1,indice,aux,op)
+        if '/' in aux : valor = dividir(exp1,indice,aux,op)
+    else : 
+      if '^' in aux : valor = potencia(exp1,indice,aux,op) 
+      if '/' in aux :valor  =  dividir(exp1,indice,aux,op)
+      elif '*' in aux : valor = multiplicar(exp1,indice,aux,op)
+   else :
+    if '^' in aux : valor = potencia(exp1,indice,aux,op)
+    elif '/' in aux : valor = dividir(exp1,indice,aux,op)
+    elif '*' in aux : valor = multiplicar(exp1,indice,aux,op)
+    elif '-' in aux :  valor = subtrair(exp1,indice,aux,op)
+    elif '+' in aux : valor = somar(exp1,indice,aux,op)
+    if len(aux) == 1 : return(aux[0]) 
+  
+def calcula(exp) :
+ expressao = exp.split()
+ valor = 0 
  while (True) :
-  if acertou==word_size : break 
-  if gameover == 0 :   readletter = mostrafase0().lower()
-  if gameover == 1 :   readletter = mostrafase1().lower()
-  if gameover == 2 :   readletter = mostrafase2().lower()
-  if gameover == 3 :   readletter = mostrafase3().lower()
-  if gameover == 4 :   readletter = mostrafase4().lower()
-  if gameover == 5 :   readletter = mostrafase5().lower()
-  if gameover == 6 :   break
-  showletter += [readletter]
-  if readletter in word_choice :  # Checa se a letra escolhida está na palavra
-      for i in range (0,word_size,1) :
-       if compare_word[i] =='_' and readletter in word_choice[i] :  # Se a letra estiver em um espaço vazio
-         compare_word[i] = readletter # preenche o espaço vazio
-         acertou+=1  
-  else : gameover += 1
-if gameover == 6 :
-  print(f'VOCE ERROU ! A PALAVRA ERA {word_choice.upper()}\n\n')
-  mostrafase6()
-else : print(f'VOCE ACERTOU ! A PALAVRA ERA ---> {word_choice.title()}\n\n')
+  if '('in expressao and ')' in expressao :
+   paren = expressao.index('(')
+   resposta = conta_especial(expressao,paren+1)
+   del(expressao[paren:expressao.index(')')+1])
+   expressao.insert(paren,resposta)
+  else : break
+ while (True) :
+  if '['in expressao and ']' in expressao :
+   paren = expressao.index('[')
+   resposta = conta_especial(expressao,paren+1)
+   del(expressao[paren:expressao.index(']')+1])
+   expressao.insert(paren,resposta)
+  else : break 
+ while (True) :
+  if '{'in expressao and '}' in expressao :
+   paren = expressao.index('{')
+   resposta = conta_especial(expressao,paren+1)
+   del(expressao[paren:expressao.index('}')+1])
+   expressao.insert(paren,resposta)
+  else : break 
+ op = 0
+ while(True) :
+
+   if '*' in expressao and '/' in expressao :
+    if expressao.index("*") < expressao.index("/") :
+        if '^' in expressao :  valor = potencia(expressao,1,1,op)
+        if '*' in expressao :  valor = multiplicar(expressao,1,1,op)
+        if '/' in expressao :  valor = dividir(expressao,1,1,op)
+    else : 
+      if '^' in expressao :  valor = potencia(expressao,1,1,op)
+      if '/' in expressao :  valor = dividir(expressao,1,1,op)
+      elif '*' in expressao :  valor =  multiplicar(expressao,1,1,op)
+   else : 
+    if '^' in expressao : valor = potencia(expressao,1,1,op)
+    elif '/' in expressao : valor =  dividir(expressao,1,1,op)
+    elif '*' in expressao : valor = multiplicar(expressao,1,1,op)
+    elif '-' in expressao : valor =  subtrair(expressao,1,1,op)
+    elif '+' in expressao : valor = somar (expressao,1,1,op)
+    elif len(expressao) == 1 : return(expressao[0]) 
+
+while (True) :
+ print ('\n\n'+'='*30 + ' CALCULADORA DE EXPRESSOES NUMÉRICAS  ' + '='*30)
+ print ('\n (*) - multiplicação entre 2 e 5 --> exemplo : 2 * 5 ')
+ print ('\n (/) - divisao entre 2 e 5 --> exemplo : 2 / 5  , se for uma fração use assim --> ( 2 / 5 )')
+ print ('\n (^) - potenciação - 2 elevado a 5 --> exemplo : 2 ^ 5 ')
+ print ('\n (+) - adição entre 2 e 5 --> exemplo : 2 + 5 ')
+ print ('\n (-) - Subtração entre 2 e 5 --> exemplo : 2 - 5 ')
+ print('\nOBSERVACAO : Todo digito do teclado deve ser separado por espaço -- > 2-4 - 2  NAO é uma expressao válida\n')
+ print("\n\n Exemplo de expressao válida : { 2 * 5 + 4 - [ ( 2 + 9 ) ] + 2 } ")
+ exp = []
+ exp = input("\nSua expressao : ") 
+ print("\n\n")
+ print(exp,'\n')
+ print(f'\n\nO valor da sua expressao é {round(calcula(exp),3)} \n')
+ novamente = input(('\n\nQuer colocar outra expressao ? [S/N] : '))
+ if novamente == 'N' :
+    break
